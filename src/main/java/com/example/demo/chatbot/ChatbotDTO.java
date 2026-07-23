856f704736780b393a4ca4fc17f6b9ec27925eef
dlwlsdn243 <dlwlsdn243@gmail.com>
2026-07-23 14:51:24 +0900

    9999

---
+++ src/main/java/com/example/demo/chatbot/ChatbotDTO.java
@@ -0,0 +1,13 @@
+package com.example.demo.chatbot;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatbotDTO {
+    private Long id;
+}
