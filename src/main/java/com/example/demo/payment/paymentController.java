05cdd9edc06cc4b8c8eafd06d7e2a37f7d3655f2
jgkim7848-lab <jgkim7848@gmail.com>
2026-07-23 14:13:32 +0900

    home12

---
+++ src/main/java/com/example/demo/payment/paymentController.java
@@ -0,0 +1,12 @@
+package com.example.demo.payment;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/example/*")
+public class paymentController {
+
+
+
+}
