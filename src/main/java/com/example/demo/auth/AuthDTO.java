0eae6b49a31b53b6fda5e2b0257637ca2cf99639
Jihun Park <115475334+chihunpak@users.noreply.github.com>
2026-07-23 14:52:46 +0900

    20260723-2

---
+++ src/main/java/com/example/demo/auth/AuthDTO.java
@@ -0,0 +1,15 @@
+package com.example.demo.auth;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthDTO {
+
+    private Long id;
+
+}
