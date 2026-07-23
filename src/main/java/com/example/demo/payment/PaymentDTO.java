50930d80c27c7fe47e4b95aa02116f121d1a9476
jgkim7848-lab <jgkim7848@gmail.com>
2026-07-23 14:51:06 +0900

    wanttogohome16

---
+++ src/main/java/com/example/demo/payment/PaymentDTO.java
@@ -0,0 +1,11 @@
+package com.example.demo.payment;
+import lombok.*;
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PaymentDTO {
+    private long id;
+}
