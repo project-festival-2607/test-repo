680076679b3cd5be1023143ed17191409858afd2
HYUN1400 <o_ovo_024@naver.com>
2026-07-23 14:12:40 +0900

    0723 festival

---
+++ src/main/java/com/example/demo/festival/FestivalController.java
@@ -0,0 +1,10 @@
+package com.example.demo.festival;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/festival/*")
+public class FestivalController {
+    // 자려고누웠는데양의지
+}
