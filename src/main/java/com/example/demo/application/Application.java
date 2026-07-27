d13198f063509c4a6ed243a33a289aed28584b28
dlwlsdn243 <dlwlsdn243@gmail.com>
2026-07-27 14:40:50 +0900

    1111

--- src/main/java/com/example/demo/application/Application.java
+++ src/main/java/com/example/demo/application/Application.java
@@ -1,8 +1,9 @@
 package com.example.demo.application;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -14,5 +15,25 @@ import lombok.*;
 public class Application {  // "Example"을 첫 글자를 대문자로 한 폴더 이름으로 바꾸기
 
   @Id
-  private Long id;
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long applyId; // 지원 아이디
+
+  @Column
+  private Long id; // uid
+
+  @Column
+  private String recruitId; // 모집공고 아이디
+
+  @Column
+  private String resumeId; // 이력서 아이디
+
+  @Column
+  private LocalDateTime registerDate; // 지원일시
+
+  @Column
+  private LocalDateTime readDate; // 열람일시
+
+  @Column
+  @Enumerated(EnumType.STRING)
+  private Application result; // 결과
 }
--- src/main/java/com/example/demo/application/ApplicationDTO.java
+++ src/main/java/com/example/demo/application/ApplicationDTO.java
@@ -2,6 +2,8 @@ package com.example.demo.application; // "example"을 폴더 이름으로 바꾸
 
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @ToString
@@ -10,6 +12,11 @@ import lombok.*;
 @Builder
 public class ApplicationDTO { // "Example"을 첫 글자를 대문자로 한 폴더 이름으로 바꾸기
 
-  private Long id;
-
+  private Long applyId; // 지원 아이디
+  private Long id; // uid
+  private String recruitId; // 모집공고 아이디
+  private String resumeId; // 이력서 아이디
+  private LocalDateTime registerDate; // 지원일시
+  private LocalDateTime readDate; // 열람일시
+  private LocalDateTime result; // 결과
 }
