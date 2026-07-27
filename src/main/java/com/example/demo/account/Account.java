b73732c4ff14d8f7fabf71bf7346b8e11cd23940
Jihun Park <115475334+chihunpak@users.noreply.github.com>
2026-07-27 14:52:04 +0900

    20260727-1

--- src/main/java/com/example/demo/account/Account.java
+++ src/main/java/com/example/demo/account/Account.java
@@ -18,12 +18,17 @@ public class Account {
 
     @Id
     private Long id;    // pk
-    private String password_hash;   // 패스워드 해시
+    @Column(name = "password_Hash", length = 100)
+    private String passwordHash;   // 패스워드 해시
+    @Column(length = 20)
     private String username;    // 아이디
+    @Column(length = 30)
     private String name;    // 이름
+    @Column(length = 20)
     private String phone;   // 전화번호
     @Column(name = "phone_verified")
     private boolean phoneVerified;  // 전화번호 인증여부
+    @Column(length = 254)
     private String email;   // 이메일
 
     @Enumerated(EnumType.STRING)
--- src/main/java/com/example/demo/account/AccountDTO.java
+++ src/main/java/com/example/demo/account/AccountDTO.java
@@ -2,6 +2,8 @@ package com.example.demo.account;
 
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @ToString
@@ -10,6 +12,16 @@ import lombok.*;
 @Builder
 public class AccountDTO {
 
-    private Long id;
-
+    private Long id;    // pk
+    private String passwordHash;   // 패스워드 해시
+    private String username;    // 아이디
+    private String name;    // 이름
+    private String phone;   // 전화번호
+    private boolean phoneVerified;  // 전화번호 인증여부
+    private String email;   // 이메일
+    private AccountType accountType; // 계정유형
+    private AccountStatus accountStatus;  // 계정상태
+    private LocalDateTime createdAt;    // 생성일
+    private LocalDateTime updatedAt;
+    private LocalDateTime deletedAt;
 }
