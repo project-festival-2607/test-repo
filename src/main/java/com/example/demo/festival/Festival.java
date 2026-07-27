d90fd2a9145dc13c2365e7e0fc1bc3bd06df3010
HYUN1400 <o_ovo_024@naver.com>
2026-07-27 14:09:44 +0900

    0727 festival

--- src/main/java/com/example/demo/festival/Festival.java
+++ src/main/java/com/example/demo/festival/Festival.java
@@ -1,8 +1,11 @@
 package com.example.demo.festival;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.*;
+
+import java.util.Date;
 
 @Entity
 @Getter
@@ -13,5 +16,24 @@ import lombok.*;
 @Builder
 public class Festival {
     @Id
-    private long id;
+    @Column(name = "content_id")
+    private long contentId;
+    private String title;
+    private String address;
+    @Column(name = "content_type")
+    private int contentType;
+    @Column(name = "start_date")
+    private Date startDate;
+    @Column(name = "end_date")
+    private Date endDate;
+    @Column(name = "map_x")
+    private int mapX;
+    @Column(name = "map_y")
+    private int mapY;
+    @Column(name = "zip_code")
+    private String zipCode;
+    @Column(name = "tel_num")
+    private String telNum;
+    @Column(name = "fes_info")
+    private String fesInfo;
 }
--- src/main/java/com/example/demo/festival/FestivalDTO.java
+++ src/main/java/com/example/demo/festival/FestivalDTO.java
@@ -2,6 +2,9 @@ package com.example.demo.festival;
 
 import lombok.*;
 
+import java.text.DecimalFormat;
+import java.util.Date;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -9,5 +12,15 @@ import lombok.*;
 @ToString
 @Builder
 public class FestivalDTO {
-    private long id;
+    private String contentId;
+    private String title;
+    private String address;
+    private int contentType;
+    private Date startDate;
+    private Date endDate;
+    private int mapX;
+    private int mapY;
+    private String zipCode;
+    private String telNum;
+    private String fesInfo;
 }
