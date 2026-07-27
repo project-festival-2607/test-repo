6db0ce4ddc848ed37653f3832cc388dd223cc70c
짜몽 <teentoplove23@gmail.con>
2026-07-27 14:19:36 +0900

    cha_11

--- src/main/java/com/example/demo/file/File.java
+++ src/main/java/com/example/demo/file/File.java
@@ -1,8 +1,12 @@
 package com.example.demo.file;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -11,8 +15,23 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Table(name = "file")
 public class File {  // "Example"을 첫 글자를 대문자로 한 폴더 이름으로 바꾸기
 
   @Id
+  @Column(name = "file_id", nullable = false)
   private Long id;
+  private Long uuid;
+  @Column(name = "file_name")
+  private String fileName;
+  @Column(name = "save_dir")
+  private String saveDir;
+  @Column(name = "file_type")
+  private int fileType;
+  @Column(name = "file_subtype")
+  private int fileSubtype;
+  @Column(name = "file_size")
+  private Long fileSize;
+  @Column(name = "uploaded_at")
+  private LocalDateTime uploadedAt;
 }
--- src/main/java/com/example/demo/file/FileDTO.java
+++ src/main/java/com/example/demo/file/FileDTO.java
@@ -1,6 +1,9 @@
 package com.example.demo.file; // "example"을 폴더 이름으로 바꾸기
 
+import jakarta.persistence.Column;
 import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -11,5 +14,11 @@ import lombok.*;
 public class FileDTO { // "Example"을 첫 글자를 대문자로 한 폴더 이름으로 바꾸기
 
   private Long id;
-
+  private Long uuid;
+  private String fileName;
+  private String saveDir;
+  private int fileType;
+  private int fileSubtype;
+  private Long fileSize;
+  private LocalDateTime uploadedAt;
 }
