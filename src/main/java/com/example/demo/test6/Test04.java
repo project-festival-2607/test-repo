808678994aedc1e8a4949e4955c2d71580c5655e
jgkim7848-lab <jgkim7848@gmail.com>
2026-07-28 14:17:46 +0900

    "feat: add() 함수 구현"

---
+++ src/main/java/com/example/demo/test6/Test04.java
@@ -0,0 +1,13 @@
+package com.example.demo.test6;
+
+
+public class Test04 {
+    static int add(int a, int b){
+        return a + b;
+    }
+    public static void main(String[] args) {
+        int result = add(3, 5);
+        System.out.println(result);
+    }
+
+}
