4e2286c61a3b3f8e34ee7361c273a80b589e99f9
dlwlsdn243 <dlwlsdn243@gmail.com>
2026-07-28 14:24:02 +0900

    feat: add() 함수 구현

--- src/main/java/com/example/demo/test6/Test.java
+++ src/main/java/com/example/demo/test6/Test.java
@@ -6,7 +6,7 @@ public class Test {
   //        구현을 마친 후 커밋 메시지를 "feat: add() 함수 구현" 으로 입력 후 commit
 
   static int add(int a, int b) {
-    return a+b;   // 여기를 구현하기
+    return 0;   // 여기를 구현하기
   }
 
   // 작업 2: 양의 정수 a, b의 최대공약수를 구하는 함수 gcd() 수정하기
@@ -14,7 +14,7 @@ public class Test {
   static int gcd(int a, int b) {
     if (b == 0) return a;
     if (a < b) return gcd(b, a);
-    return gcd(b, a % b);     // a % a를 a % b로 수정
+    return gcd(b, a % a);     // a % a를 a % b로 수정
   }
 
   public static void main(String[] args){
