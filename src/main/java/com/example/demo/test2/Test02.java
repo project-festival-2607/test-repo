1edb17b9f2efa3f074bcfba13ee96f34bb150427
rooguie <rooguie@naver.com>
2026-07-28 14:28:26 +0900

    fix: gcd() 함수 수정

--- src/main/java/com/example/demo/test2/Test02.java
+++ src/main/java/com/example/demo/test2/Test02.java
@@ -14,7 +14,7 @@ public class Test02 {
     static int gcd(int a, int b) {
         if (b == 0) return a;
         if (a < b) return gcd(b, a);
-        return gcd(b, a % a);     // a % a를 a % b로 수정
+        return gcd(b, a % b);     // a % a를 a % b로 수정
     }
 
     public static void main(String[] args){
